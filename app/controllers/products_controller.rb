4649f535f257e0538267775e46d33357e8a054e9
Camden <camdencparker@gmail.com>
2022-01-12 15:04:07 -0700

    adds url segment params to access db

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,15 @@
 class ProductsController < ApplicationController
+  def available_products
+    product_arr = Product.all
+    product_id = params[:id].to_i
+    selected_prod = product_arr[product_id]
+    
+    render json: {message: selected_prod}
+  end
   
+  def first_product
+    first = Product.first
+    render json: first.as_json
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get "/all_products" => "products#available_products"
+
+  get "/all_products/:name" => "products#available_products"
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
