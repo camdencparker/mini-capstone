a4efda87aa5450780469166b154d4f64f4762fee
Camden <camdencparker@gmail.com>
2022-01-12 11:33:05 -0700

    seeds two new instances to the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Product.create(name: "Clown Fish", price: 45, image_url: "fishasdf", description: "a tiny fish that tells hilarious jokes and lives in an anemone")
+Product.create(name: "Lion Fish", price: 80, image_url: "aaaaddddd", description: "a huge fish willing to swallow anything that will fit in its mouth")
