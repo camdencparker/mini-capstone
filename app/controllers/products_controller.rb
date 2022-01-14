e639ae16db70e43bb0062ae1386b2416b1d297b9
Camden <camdencparker@gmail.com>
2022-01-14 13:05:56 -0700

    adds delete request to app with proper pathing

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,19 +4,39 @@ class ProductsController < ApplicationController
     render json: list_all.as_json
   end
 
+  def create
+    product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description]
+    )
+    product.save
+    render json: product.as_json
+  end
+   
+
   def show
     id = params[:id]
     show = Product.find(id)
     render json: show
   end
 
-  def create
-    fish = Product.new(
-      name: params[:name],
-      price: params[:price],
-      image_url: params[:image_url]
-    )
-    fish.save
-    render json: fish.as_json
+  def update
+    product = Product.find(params[:id])
+    product.name = params[:name] || product.name
+    product.price = params[:price] || product.price
+    product.image_url = params[:image_url] || product.image_url
+    product.description = params[:description] || product.description
+    product.save
+    render json: product.as_json
   end
+
+  def delete
+    product = Product.find(params[:id])
+    product.destroy
+    render json: {message: "Product is gone into the dark night never to be found again"}
+  end
+
+  
 end
--- misc_ruby.rb
+++ misc_ruby.rb
@@ -3,3 +3,9 @@ require "http"
 index = HTTP.get("http://localhost:3000/products")
 
 show = HTTP.get("http://localhost:3000/products/3")
+
+create = HTTP.post("http://localhost:3000/products", :json => {:name => "Spotted Grouper", :price => "100", :description => "This fish likes to stare its prey down"})
+
+update = HTTP.patch("http://localhost:3000/products")
+
+destroy = HTTP.destroy("http://localhost:3000/products/3")
