bc78a085a4717c53d215d2f8a7ed9a8cae86ce72
Camden <camdencparker@gmail.com>
2022-01-14 10:31:17 -0700

    cleaned up code

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,17 +1,4 @@
 class ProductsController < ApplicationController
-  def available_products
-    product_arr = Product.all
-    product_id = params[:id].to_i
-    selected_prod = product_arr[product_id]
-    
-    render json: {message: selected_prod}
-  end
-  
-  def first_product
-    first = Product.first
-    render json: first.as_json
-  end
-
   def index
     list_all = Product.all
     render json: list_all.as_json
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  get "/all_products" => "products#available_products"
-
-  get "/all_products/:name" => "products#available_products"
-
   get "/products" => "products#index"
 
   get "/products/:id" => "products#show"
-  
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
